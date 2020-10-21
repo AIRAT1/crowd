e3e278fcc99946ae9f87b1dcb083749872e71401
Ayrat <ayrat1@mail.ru>
2020-10-21 22:27:55 +0300

    add init values in db

--- src/main/java/de/crowd/CrowdApplication.java
+++ src/main/java/de/crowd/CrowdApplication.java
@@ -1,13 +1,28 @@
 package de.crowd;
 
+import de.crowd.domain.Employee;
+import de.crowd.repo.EmployeeRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrowdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrowdApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(EmployeeRepo repository) {
+		return (args) -> {
+			repository.save(new Employee("Jack", "Bauer", ""));
+			repository.save(new Employee("Chloe", "O'Brian", ""));
+			repository.save(new Employee("Kim", "Bauer", ""));
+			repository.save(new Employee("David", "Palmer", ""));
+			repository.save(new Employee("Michelle", "Dessler", ""));
+		};
 	}
 
 }
