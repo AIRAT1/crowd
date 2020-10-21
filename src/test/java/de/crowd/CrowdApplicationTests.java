3dd0a8067762e6e453bb3adb7c896e3ffacf367c
Ayrat <ayrat1@mail.ru>
2020-10-21 22:15:38 +0300

    init project

---
+++ src/test/java/de/crowd/CrowdApplicationTests.java
@@ -0,0 +1,13 @@
+package de.crowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
